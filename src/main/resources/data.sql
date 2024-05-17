5b76c6ae11a1eeefad341032c6b99e3e13a20c86
Doyeop Kim <dygames34@gmail.com>
2024-05-17 16:37:40 +0900

    fix: mysql data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,42 +1,42 @@
-INSERT INTO MEMBER values(0, 'dygames@gmail.com', 'dygames', 'dy1234');
-INSERT INTO MEMBER values(1, 'dygames1@gmail.com', 'dygames1', 'dy1234');
+INSERT INTO MEMBER values(1, 'dygames@gmail.com', 'dygames', 'dy1234');
+INSERT INTO MEMBER values(2, 'dygames1@gmail.com', 'dygames1', 'dy1234');
 
-INSERT INTO CATEGORY values(0, 'Programming');
-INSERT INTO CATEGORY values(1, 'Fashion');
-INSERT INTO CATEGORY values(2, 'Science');
+INSERT INTO CATEGORY values(1, 'Programming');
+INSERT INTO CATEGORY values(2, 'Fashion');
+INSERT INTO CATEGORY values(3, 'Science');
 
-INSERT INTO QUESTION_HEADERS values(0);
+INSERT INTO QUESTION_HEADERS values(1);
 
-INSERT INTO QUESTION_HEADER values(0, 0, 'LONG', 'Description 1', 'Long Question Hint', 'Long Question');
-INSERT INTO QUESTION_HEADER values(1, 0, 'SHORT', 'Description 2', 'Short Question Hint', 'Short Question');
-INSERT INTO QUESTION_HEADER values(2, 0, 'MULTIPLE_CHOICE', 'Description 3', null, 'Multiple Choice Question');
+INSERT INTO QUESTION_HEADER values(1, 1, 'LONG', 'Description 1', 'Long Question Hint', 'Long Question');
+INSERT INTO QUESTION_HEADER values(2, 1, 'SHORT', 'Description 2', 'Short Question Hint', 'Short Question');
+INSERT INTO QUESTION_HEADER values(3, 1, 'MULTIPLE_CHOICE', 'Description 3', null, 'Multiple Choice Question');
 
-INSERT INTO CHOICE values(0, 2, 'Choice 1');
-INSERT INTO CHOICE values(1, 2, 'Choice 2');
-INSERT INTO CHOICE values(2, 2, 'Choice 3');
+INSERT INTO CHOICE values(1, 3, 'Choice 1');
+INSERT INTO CHOICE values(2, 3, 'Choice 2');
+INSERT INTO CHOICE values(3, 3, 'Choice 3');
 
-INSERT INTO ARTICLE values(0, 0, {ts '2012-09-17 18:47:52.69'}, 0, 0, 'Article Body', 'Article Title');
+INSERT INTO ARTICLE values(1, 1, {ts '2012-09-17 18:47:52.69'}, 1, 1, 'Article Body', 'Article Title');
 
-INSERT INTO ANSWER values(0, 0, 0);
-INSERT INTO QUESTION_BODY values(0, 0, 'LONG', 'Long Answer 1');
-INSERT INTO QUESTION_BODY values(1, 1, 'SHORT', 'Short Answer 1');
-INSERT INTO QUESTION_BODY values(2, 2, 'MULTIPLE_CHOICE', '1');
-INSERT INTO ANSWER_QUESTION_BODIES values(0, 0);
-INSERT INTO ANSWER_QUESTION_BODIES values(0, 1);
-INSERT INTO ANSWER_QUESTION_BODIES values(0, 2);
-
-INSERT INTO ANSWER values(0, 0, 1);
-INSERT INTO QUESTION_BODY values(3, 0, 'LONG', 'Long Answer 2');
-INSERT INTO QUESTION_BODY values(4, 1, 'SHORT', 'Short Answer 2');
-INSERT INTO QUESTION_BODY values(5, 2, 'MULTIPLE_CHOICE', '2');
+INSERT INTO ANSWER values(1, 1, 1);
+INSERT INTO QUESTION_BODY values(1, 1, 'LONG', 'Long Answer 1');
+INSERT INTO QUESTION_BODY values(2, 2, 'SHORT', 'Short Answer 1');
+INSERT INTO QUESTION_BODY values(3, 3, 'MULTIPLE_CHOICE', '1');
+INSERT INTO ANSWER_QUESTION_BODIES values(1, 1);
+INSERT INTO ANSWER_QUESTION_BODIES values(1, 2);
 INSERT INTO ANSWER_QUESTION_BODIES values(1, 3);
-INSERT INTO ANSWER_QUESTION_BODIES values(1, 4);
-INSERT INTO ANSWER_QUESTION_BODIES values(1, 5);
 
-INSERT INTO ANSWER values(0, 1, 2);
-INSERT INTO QUESTION_BODY values(6, 0, 'LONG', 'Long Answer 3');
-INSERT INTO QUESTION_BODY values(7, 1, 'SHORT', 'Short Answer 3');
-INSERT INTO QUESTION_BODY values(8, 2, 'MULTIPLE_CHOICE', '3');
+INSERT INTO ANSWER values(1, 1, 2);
+INSERT INTO QUESTION_BODY values(4, 1, 'LONG', 'Long Answer 2');
+INSERT INTO QUESTION_BODY values(5, 2, 'SHORT', 'Short Answer 2');
+INSERT INTO QUESTION_BODY values(6, 3, 'MULTIPLE_CHOICE', '2');
+INSERT INTO ANSWER_QUESTION_BODIES values(2, 4);
+INSERT INTO ANSWER_QUESTION_BODIES values(2, 5);
 INSERT INTO ANSWER_QUESTION_BODIES values(2, 6);
-INSERT INTO ANSWER_QUESTION_BODIES values(2, 7);
-INSERT INTO ANSWER_QUESTION_BODIES values(2, 8);
+
+INSERT INTO ANSWER values(1, 2, 3);
+INSERT INTO QUESTION_BODY values(7, 1, 'LONG', 'Long Answer 3');
+INSERT INTO QUESTION_BODY values(8, 2, 'SHORT', 'Short Answer 3');
+INSERT INTO QUESTION_BODY values(9, 3, 'MULTIPLE_CHOICE', '3');
+INSERT INTO ANSWER_QUESTION_BODIES values(3, 7);
+INSERT INTO ANSWER_QUESTION_BODIES values(3, 8);
+INSERT INTO ANSWER_QUESTION_BODIES values(3, 9);
